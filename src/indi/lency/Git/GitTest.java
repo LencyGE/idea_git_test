e324379426799c251ae96b6f21e4da18aad3cdc5
Lency <Lencygmc@foxmail.com>
2021-09-09 11:25:04 +0800

    添加文件

---
+++ src/indi/lency/Git/GitTest.java
@@ -0,0 +1,23 @@
+/**
+ * Author: Lency
+ * Encoding: UTF-8
+ * Project: gitworkspace
+ * File: GitTest.py
+ * Ide: IntelliJ IDEA
+ * Create Time: 2021-09-09 11-10-29
+ * Description:
+ * Version: V1.0
+ * <p>
+ * Modification  History:
+ * Date         Author        Version        Description
+ * 1970         Lency           1.0             1.0
+ * Why & What is modified:
+ */
+
+package indi.lency.Git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
