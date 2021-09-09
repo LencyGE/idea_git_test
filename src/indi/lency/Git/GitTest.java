04400f704238edfdba216376df198160d96df4dd
Lency <Lencygmc@foxmail.com>
2021-09-09 11:41:26 +0800

    创建分支B

--- src/indi/lency/Git/GitTest.java
+++ src/indi/lency/Git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("Hello Git!");
         System.out.println("更新1");
         System.out.println("创建分支A");
+        System.out.println("分支B");
     }
 }
